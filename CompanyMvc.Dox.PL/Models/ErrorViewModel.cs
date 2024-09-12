8e8421e054ed340f1b0114c1a02698fc492f2a26
Hazem Fady <hazemfady123@gmail.com>
2024-09-12 18:38:05 +0300

    Add project files.

---
+++ CompanyMvc.Dox.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CompanyMvc.Dox.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
