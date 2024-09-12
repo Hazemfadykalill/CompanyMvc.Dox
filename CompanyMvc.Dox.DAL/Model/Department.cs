0c8e11e69606039629d93d37bd6eb26145a6f95b
Hazem Fady <hazemfady123@gmail.com>
2024-09-12 19:03:50 +0300

    DAL For Department

---
+++ CompanyMvc.Dox.DAL/Model/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyMvc.Dox.DAL.Model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfCreation{ get; set; }
+    }
+}
