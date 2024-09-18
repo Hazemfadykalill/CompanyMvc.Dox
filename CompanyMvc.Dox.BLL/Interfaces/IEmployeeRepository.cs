51e0ae5f32c1342ed676424cc687cd60707dbfb0
Hazem Fady <hazemfady123@gmail.com>
2024-09-19 01:49:21 +0300

    BLL ( Employee )

---
+++ CompanyMvc.Dox.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using CompanyMvc.Dox.DAL.Model;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyMvc.Dox.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+      
+
+
+        IEnumerable<Employee> GetAll();
+
+        //Get Employee By Id
+        Employee GetById(int? id);
+
+        //Add Employee
+        int Add(Employee department);
+        //Update Employee
+
+        int Update(Employee department);
+        //Delete Employee
+
+        int Remove(Employee employee);
+    }
+}
---
+++ CompanyMvc.Dox.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,49 @@
+﻿using CompanyMvc.Dox.BLL.Interfaces;
+using CompanyMvc.Dox.DAL.Data.Contexts;
+using CompanyMvc.Dox.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyMvc.Dox.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public EmployeeRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return dbContext.Employees.ToList();
+        }
+        public Employee GetById(int? id)
+        {
+           return dbContext.Employees.Find(id);
+        }
+        public int Add(Employee employee)
+        {
+            dbContext.Employees.Add(employee);
+            return dbContext.SaveChanges();
+           
+        }
+
+        public int Update(Employee employee)
+        {
+            dbContext.Employees.Update(employee);
+            return dbContext.SaveChanges();
+        }
+
+
+        public int Remove(Employee employee)
+        {
+            dbContext.Employees.Remove(employee);
+            return dbContext.SaveChanges();
+        }
+
+    }
+}
