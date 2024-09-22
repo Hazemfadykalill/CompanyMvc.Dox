6fdd407fd90be9eb6ddec843aa1c885adaeb1d2f
Hazem Fady <hazemfady123@gmail.com>
2024-09-22 20:10:20 +0300

    Deleting File

--- CompanyMvc.Dox.PL/HelperLogic/DocumentSettings.cs
+++ CompanyMvc.Dox.PL/HelperLogic/DocumentSettings.cs
@@ -2,6 +2,7 @@
 {
     public static class DocumentSettings
     {
+        //Uploading
         public static string UploadingFile(IFormFile file,string FolderName)
         {
 
@@ -10,17 +11,28 @@
             // in previous Line Is Static In Your PC So That We Need Make Dynamic
             //string FilePath = Directory.GetCurrentDirectory() + @"\wwwroot\Files\" + FileName;
             //or  + is Combine
-           string FolderPath = Path.Combine(Directory.GetCurrentDirectory() ,@"\wwwroot\Files\" , FolderName);
+           string FolderPath = Path.Combine(Directory.GetCurrentDirectory() ,@"wwwroot\Files" , FolderName);
 
             //2. Get File Name
             string FileName = $"{Guid.NewGuid()}{file.FileName}";
             //3 Get Location File (IMG,PDF ,video)
             string FilePath=Path.Combine(FolderPath,FileName);
             //4 save file as stream
-            var FileStream = new FileStream(FilePath,FileMode.Create);
+           using var FileStream = new FileStream(FilePath,FileMode.Create);
             file.CopyTo(FileStream);
 
             return FileName;
+        }
+
+
+        //Deleting
+        public static void DeletingFile(string FolderName,string FileName)
+        {
+            // 1. get FilePath (folderPath + filename)
+           string FilePath = Path.Combine(Directory.GetCurrentDirectory() ,@"wwwroot\Files" , FolderName,FileName);
+            // 2. I am Delete This File But Should Know this file exists yes Or No
+            if(File.Exists(FilePath))
+                File.Delete(FilePath);
         }
     }
 }
