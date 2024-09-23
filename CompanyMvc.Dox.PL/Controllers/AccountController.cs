279984b65071d56e5749aa35f23e0b003b291ca2
Hazem Fady <hazemfady123@gmail.com>
2024-09-23 05:18:10 +0300

    Login Controller And View

--- CompanyMvc.Dox.PL/Controllers/AccountController.cs
+++ CompanyMvc.Dox.PL/Controllers/AccountController.cs
@@ -5,30 +5,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyMvc.Dox.PL.Controllers
 {
-    public class AccountController : Controller
-    {
+	public class AccountController : Controller
+	{
 		private readonly UserManager<ApplicationUser> userManger;
 
 		public AccountController(UserManager<ApplicationUser> userManger)
-        {
+		{
 			this.userManger = userManger;
 		}
-        //SignUp
-        [HttpGet]
-        public IActionResult SignUp()
-        {
-            return View();
-        }
+		//SignUp
+		[HttpGet]
+		public IActionResult SignUp()
+		{
+			return View();
+		}
 
 
 		//SignUp
 		[HttpPost]
 		public async Task<IActionResult> SignUp(SignUpViewModel model)
 		{
-            if (ModelState.IsValid)
-            {
-                try
-                {
+			if (ModelState.IsValid)
+			{
+				try
+				{
 					var User = await userManger.FindByNameAsync(model.UserName);
 					if (User is null)
 					{
@@ -59,26 +59,65 @@ namespace CompanyMvc.Dox.PL.Controllers
 
 						}
 						ModelState.AddModelError(string.Empty, "This Email Is Already Exists!!");
-						return View(model);	
+						return View(model);
 					}
 					ModelState.AddModelError(string.Empty, "This Email Is Already Exists!!");
 
 				}
-				catch(Exception ex)
-                {
+				catch (Exception ex)
+				{
 					ModelState.AddModelError(string.Empty, ex.Message);
 
-                }
-            }
+				}
+			}
 			return View(model);
 		}
 		//Login
 		[HttpGet]
-        public IActionResult Login()
-        {
-            return View();
-        }
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		//Login
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					
+
+					var User =await userManger.FindByEmailAsync(model.Email);
+					if (User is not  null)
+					{
+					
+						//Password
+						var Result =await  userManger.CheckPasswordAsync(User, model.Password);
+						if (Result)
+						{
+							//login
+							return RedirectToAction("Index", "Home");
+
+						}
 
 
-    }
+					}
+
+					ModelState.AddModelError(string.Empty, "Invalid Login !!");
+
+
+				}
+				catch (Exception ex)
+				{
+					ModelState.AddModelError(string.Empty, ex.Message);
+				}
+			}
+			return View(model);
+		}
+
+
+
+	}
 }
---
+++ CompanyMvc.Dox.PL/ViewModels/Auth/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyMvc.Dox.PL.ViewModels.Auth
+{
+	public class LoginViewModel
+	{
+
+		
+
+		
+		[Required(ErrorMessage = "Please Enter Your Email!!")]
+		[EmailAddress]
+		public string Email { get; set; }
+		[Required(ErrorMessage = "Please Enter Your Password!!")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		public bool RememberMe { get; set; }
+
+	}
+}
