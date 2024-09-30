d036f97f77ab999ee9cc3abd92d3299ea2598d69
Hazem Fady <hazemfady123@gmail.com>
2024-09-30 06:52:38 +0300

    Update On User Controllers And Views

--- CompanyMvc.Dox.PL/ViewModels/Users/UserViewModel.cs
+++ CompanyMvc.Dox.PL/ViewModels/Users/UserViewModel.cs
@@ -3,10 +3,10 @@
 	public class UserViewModel
 	{
 
-        public string Id { get; set; }
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public string Email { get; set; }
-        public IEnumerable<string> Roles { get; set; }
+        public string? Id { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? Email { get; set; }
+        public IEnumerable<string>? Roles { get; set; }
 	}
 }
